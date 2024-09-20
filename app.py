e28920f68cd404d103c9563f7bd01c01801e91f5
Piotr Guzera <piotrg@voyager.pl>
2024-09-20 22:17:39 +0200

    druga wersja

--- app.py
+++ app.py
@@ -1,37 +1,68 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-# Funkcja do wyświetlania zawartości podstron
-def display_page(title, content):
-    st.title(title)
-    st.write(content)
+# Funkcje do wyświetlania zawartości strony głównej i podstron
+def strona_glowna():
+    st.title("Tytuł strony głównej")
+    st.write("Treść strony głównej")
 
-# Główna nawigacja z trzema opcjami
-menu = st.selectbox('Menu', ['Strona główna', 'Opcja 1', 'Opcja 2', 'Opcja 3'])
+def podstrona(tytul, tresc):
+    st.title(tytul)
+    st.write(tresc)
 
-# Zawartość dla każdej opcji
-if menu == 'Strona główna':
-    display_page('Tytuł strony głównej', 'Treść strony głównej')
-elif menu == 'Opcja 1':
-    submenu = st.selectbox('Podmenu Opcja 1', ['Podstrona 1.1', 'Podstrona 1.2', 'Podstrona 1.3'])
-    if submenu == 'Podstrona 1.1':
-        display_page('Tytuł podstrony 1.1', 'Treść podstrony 1.1')
-    elif submenu == 'Podstrona 1.2':
-        display_page('Tytuł podstrony 1.2', 'Treść podstrony 1.2')
-    elif submenu == 'Podstrona 1.3':
-        display_page('Tytuł podstrony 1.3', 'Treść podstrony 1.3')
-elif menu == 'Opcja 2':
-    submenu = st.selectbox('Podmenu Opcja 2', ['Podstrona 2.1', 'Podstrona 2.2', 'Podstrona 2.3'])
-    if submenu == 'Podstrona 2.1':
-        display_page('Tytuł podstrony 2.1', 'Treść podstrony 2.1')
-    elif submenu == 'Podstrona 2.2':
-        display_page('Tytuł podstrony 2.2', 'Treść podstrony 2.2')
-    elif submenu == 'Podstrona 2.3':
-        display_page('Tytuł podstrony 2.3', 'Treść podstrony 2.3')
-elif menu == 'Opcja 3':
-    submenu = st.selectbox('Podmenu Opcja 3', ['Podstrona 3.1', 'Podstrona 3.2', 'Podstrona 3.3'])
-    if submenu == 'Podstrona 3.1':
-        display_page('Tytuł podstrony 3.1', 'Treść podstrony 3.1')
-    elif submenu == 'Podstrona 3.2':
-        display_page('Tytuł podstrony 3.2', 'Treść podstrony 3.2')
-    elif submenu == 'Podstrona 3.3':
-        display_page('Tytuł podstrony 3.3', 'Treść podstrony 3.3')
+# Menu główne z trzema opcjami
+with st.sidebar:
+    opcja = option_menu(
+        menu_title=None,
+        options=["Opcja 1", "Opcja 2", "Opcja 3"],
+        icons=["list", "list", "list"],  # Możesz dostosować ikony
+        orientation="horizontal",
+        default_index=0,
+    )
+
+# Rozwijane podmenu dla każdej opcji
+if opcja == "Opcja 1":
+    podopcja = option_menu(
+        menu_title="Wybierz podmenu:",
+        options=["Podopcja 1.1", "Podopcja 1.2", "Podopcja 1.3"],
+        icons=["caret-right", "caret-right", "caret-right"],
+        orientation="vertical"
+    )
+    if podopcja == "Podopcja 1.1":
+        podstrona("Tytuł podstrony 1.1", "Treść podstrony 1.1")
+    elif podopcja == "Podopcja 1.2":
+        podstrona("Tytuł podstrony 1.2", "Treść podstrony 1.2")
+    elif podopcja == "Podopcja 1.3":
+        podstrona("Tytuł podstrony 1.3", "Treść podstrony 1.3")
+
+elif opcja == "Opcja 2":
+    podopcja = option_menu(
+        menu_title="Wybierz podmenu:",
+        options=["Podopcja 2.1", "Podopcja 2.2", "Podopcja 2.3"],
+        icons=["caret-right", "caret-right", "caret-right"],
+        orientation="vertical"
+    )
+    if podopcja == "Podopcja 2.1":
+        podstrona("Tytuł podstrony 2.1", "Treść podstrony 2.1")
+    elif podopcja == "Podopcja 2.2":
+        podstrona("Tytuł podstrony 2.2", "Treść podstrony 2.2")
+    elif podopcja == "Podopcja 2.3":
+        podstrona("Tytuł podstrony 2.3", "Treść podstrony 2.3")
+
+elif opcja == "Opcja 3":
+    podopcja = option_menu(
+        menu_title="Wybierz podmenu:",
+        options=["Podopcja 3.1", "Podopcja 3.2", "Podopcja 3.3"],
+        icons=["caret-right", "caret-right", "caret-right"],
+        orientation="vertical"
+    )
+    if podopcja == "Podopcja 3.1":
+        podstrona("Tytuł podstrony 3.1", "Treść podstrony 3.1")
+    elif podopcja == "Podopcja 3.2":
+        podstrona("Tytuł podstrony 3.2", "Treść podstrony 3.2")
+    elif podopcja == "Podopcja 3.3":
+        podstrona("Tytuł podstrony 3.3", "Treść podstrony 3.3")
+
+# Domyślnie strona główna
+if opcja == "Opcja 1" and 'podopcja' not in locals():
+    strona_glowna()
